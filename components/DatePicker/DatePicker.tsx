f15759f6761433f53596b3bd4e9fd747db6ce11f
Lucas Fuoco <fuoco.appdev@gmail.com>
2024-10-24 11:16:22 -0400

    Remove date picker styling

--- components/DatePicker/DatePicker.tsx
+++ components/DatePicker/DatePicker.tsx
@@ -229,15 +229,7 @@ function DatePicker({
         customLightStyles={customExtraLightStyles.formLayout}
       >
         <Button
-          customStyles={{
-            ...{
-              button: {
-                paddingTop: MarginsPaddings.mp_4,
-                paddingBottom: MarginsPaddings.mp_4,
-              },
-            },
-            ...customExtraStyles.button,
-          }}
+          customStyles={customExtraStyles.button}
           customDarkStyles={customExtraDarkStyles.button}
           customLightStyles={customExtraLightStyles.button}
           type={'default'}
