3c9279523c6521fef264fbf5a3137c2b057e3e56
Lucas Fuoco <fuoco.appdev@gmail.com>
2024-12-01 14:00:31 -0500

    Fix scroll view gesture in bottom sheet

--- components/BottomSheet/BottomSheet.tsx
+++ components/BottomSheet/BottomSheet.tsx
@@ -271,7 +271,7 @@ function BottomSheet({
                       }, 75);
                     }}
                   >
-                    <KeyboardAvoidingView behavior={'position'}>
+                    <KeyboardAvoidingView behavior={'height'}>
                       {children}
                     </KeyboardAvoidingView>
                   </View>
