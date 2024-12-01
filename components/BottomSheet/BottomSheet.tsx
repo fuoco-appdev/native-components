ae3e9180b991b0ad3f36ddfc5b9fec16460b2633
Lucas Fuoco <fuoco.appdev@gmail.com>
2024-12-01 17:33:07 -0500

    Fix scroll view gesture in bottom sheet

--- components/BottomSheet/BottomSheet.tsx
+++ components/BottomSheet/BottomSheet.tsx
@@ -238,76 +238,76 @@ function BottomSheet({
                 sheetAnimatedStyle,
               ]}
             >
-              <SafeAreaView>
-                {type === 'scroll-view' && (
-                  <ScrollView
-                    keyboardShouldPersistTaps={'always'}
-                    simultaneousHandlers={[panGestureRef]}
-                    contentContainerStyle={[
-                      ...(isDarkTheme
-                        ? [
-                            {
-                              ...darkStyles?.scrollView,
-                              ...(customDarkStyles?.scrollView ?? {}),
-                            },
-                          ]
-                        : [
-                            {
-                              ...lightStyles?.scrollView,
-                              ...(customLightStyles?.scrollView ?? {}),
-                            },
-                          ]),
-                      {
-                        ...styles.scrollView,
-                        ...(customStyles?.scrollView ?? {}),
-                      },
-                    ]}
-                  >
-                    <View
-                      onLayout={(e) => {
-                        sheetHeight.value = e.nativeEvent.layout.height;
-                        setTimeout(() => {
-                          translateY.value = withTiming(0, {
-                            easing: Easing.bezier(0.4, 0.0, 0.2, 1),
-                            duration: duration,
-                          });
-                        }, 75);
-                      }}
+              <KeyboardAvoidingView behavior={'height'}>
+                <SafeAreaView>
+                  {type === 'scroll-view' && (
+                    <ScrollView
+                      keyboardShouldPersistTaps={'always'}
+                      simultaneousHandlers={[panGestureRef]}
+                      contentContainerStyle={[
+                        ...(isDarkTheme
+                          ? [
+                              {
+                                ...darkStyles?.scrollView,
+                                ...(customDarkStyles?.scrollView ?? {}),
+                              },
+                            ]
+                          : [
+                              {
+                                ...lightStyles?.scrollView,
+                                ...(customLightStyles?.scrollView ?? {}),
+                              },
+                            ]),
+                        {
+                          ...styles.scrollView,
+                          ...(customStyles?.scrollView ?? {}),
+                        },
+                      ]}
                     >
-                      <KeyboardAvoidingView behavior={'height'}>
+                      <View
+                        onLayout={(e) => {
+                          sheetHeight.value = e.nativeEvent.layout.height;
+                          setTimeout(() => {
+                            translateY.value = withTiming(0, {
+                              easing: Easing.bezier(0.4, 0.0, 0.2, 1),
+                              duration: duration,
+                            });
+                          }, 75);
+                        }}
+                      >
                         {children}
-                      </KeyboardAvoidingView>
-                    </View>
-                  </ScrollView>
-                )}
-                {type === 'flat-list' && (
-                  <FlatList
-                    simultaneousHandlers={[panGestureRef]}
-                    contentContainerStyle={[
-                      ...(isDarkTheme
-                        ? [
-                            {
-                              ...darkStyles?.scrollView,
-                              ...(customDarkStyles?.scrollView ?? {}),
-                            },
-                          ]
-                        : [
-                            {
-                              ...lightStyles?.scrollView,
-                              ...(customLightStyles?.scrollView ?? {}),
-                            },
-                          ]),
-                      {
-                        ...styles.scrollView,
-                        ...(customStyles?.scrollView ?? {}),
-                      },
-                    ]}
-                    data={data}
-                    renderItem={renderItem}
-                    keyExtractor={keyExtractor}
-                  />
-                )}
-              </SafeAreaView>
+                      </View>
+                    </ScrollView>
+                  )}
+                  {type === 'flat-list' && (
+                    <FlatList
+                      simultaneousHandlers={[panGestureRef]}
+                      contentContainerStyle={[
+                        ...(isDarkTheme
+                          ? [
+                              {
+                                ...darkStyles?.scrollView,
+                                ...(customDarkStyles?.scrollView ?? {}),
+                              },
+                            ]
+                          : [
+                              {
+                                ...lightStyles?.scrollView,
+                                ...(customLightStyles?.scrollView ?? {}),
+                              },
+                            ]),
+                        {
+                          ...styles.scrollView,
+                          ...(customStyles?.scrollView ?? {}),
+                        },
+                      ]}
+                      data={data}
+                      renderItem={renderItem}
+                      keyExtractor={keyExtractor}
+                    />
+                  )}
+                </SafeAreaView>
+              </KeyboardAvoidingView>
             </Animated.View>
           </GestureDetector>
         </View>
