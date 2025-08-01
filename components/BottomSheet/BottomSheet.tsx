60700448bcde6487ceb3ae78102018bf57a6c97d
Lucas Fuoco <fuoco.appdev@gmail.com>
2025-08-01 14:13:37 -0400

    Fix bottom sheet

--- components/BottomSheet/BottomSheet.tsx
+++ components/BottomSheet/BottomSheet.tsx
@@ -346,7 +346,7 @@ function BottomSheet({
                 ...(customStyles?.sheet ?? {}),
                 ...(defaultSheetHeight && {
                   height: defaultSheetHeight,
-                  maxHeight: defaultSheetHeight,
+                  minHeight: defaultSheetHeight,
                 }),
               },
             ]}
